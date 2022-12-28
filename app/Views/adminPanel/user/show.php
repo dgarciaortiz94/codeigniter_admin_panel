a6efc7704a743110c474e35bbdb4d9a2a99dc791
dgarciaortiz94 <dgarciaortiz94@gmail.com>
2022-12-28 20:28:24 +0100

    fix some responsive details about show cards

--- app/Views/adminPanel/user/show.php
+++ app/Views/adminPanel/user/show.php
@@ -9,35 +9,37 @@
         <div class="d-flex justify-content-center">
             <div class="user-card__profile-img" style="background-image: url(<?=base_url().'/media/users/'.session('user')->image?>);"></div>
         </div>
-        <table class="table">
-            <tr>
-                <th>Nombre</th>
-                <td><?=$user->name?></td>
-            </tr>
-            <tr>
-                <th>Primer apellido</th>
-                <td><?=$user->firstname?></td>
-            </tr>
-            <tr>
-                <th>Segundo apellido</th>
-                <td><?=$user->lastname?></td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td><?=$user->email?></td>
-            </tr>
-            <tr>
-                <th>Role</th>
-                <td><?=$user->role?></td>
-            </tr>
-            <tr>
-                <th>Registro</th>
-                <td><?=$user->created_at?></td>
-            </tr>
-            <tr>
-                <th>Actualizado</th>
-                <td><?=$user->updated_at?></td>
-            </tr>
-        </table>
+        <div class="table-wrapper">
+            <table class="table">
+                <tr>
+                    <th>Nombre</th>
+                    <td><?=$user->name?></td>
+                </tr>
+                <tr>
+                    <th>Primer apellido</th>
+                    <td><?=$user->firstname?></td>
+                </tr>
+                <tr>
+                    <th>Segundo apellido</th>
+                    <td><?=$user->lastname?></td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td><?=$user->email?></td>
+                </tr>
+                <tr>
+                    <th>Role</th>
+                    <td><?=$user->role?></td>
+                </tr>
+                <tr>
+                    <th>Registro</th>
+                    <td><?=$user->created_at?></td>
+                </tr>
+                <tr>
+                    <th>Actualizado</th>
+                    <td><?=$user->updated_at?></td>
+                </tr>
+            </table>
+        </div>
     </div>
 <?= $this->endSection() ?>
--- app/Views/adminPanel/video/show.php
+++ app/Views/adminPanel/video/show.php
@@ -7,29 +7,31 @@
 <?= $this->section('section') ?>
     <div class="user-card mt-5 bg-warning">
         <div class="d-flex justify-content-center">
-            <video src="<?=base_url().'/media/videos/'.$video->path?>" controls></video>
+            <video class="w-100" src="<?=base_url().'/media/videos/'.$video->path?>" controls></video>
         </div>
-        <table class="table">
-            <tr>
-                <th>Título</th>
-                <td><?=$video->name?></td>
-            </tr>
-            <tr>
-                <th>Descripción</th>
-                <td><?=$video->description?></td>
-            </tr>
-            <tr>
-                <th>Ruta</th>
-                <td><?=$video->path?></td>
-            </tr>
-            <tr>
-                <th>Creación</th>
-                <td><?=$video->created_at?></td>
-            </tr>
-            <tr>
-                <th>Actualizado</th>
-                <td><?=$video->updated_at?></td>
-            </tr>
-        </table>
+        <div class="table-wrapper">
+            <table class="table mt-3">
+                <tr>
+                    <th>Título</th>
+                    <td><?=$video->title?></td>
+                </tr>
+                <tr>
+                    <th>Descripción</th>
+                    <td><?=$video->description?></td>
+                </tr>
+                <tr>
+                    <th>Ruta</th>
+                    <td><?=$video->path?></td>
+                </tr>
+                <tr>
+                    <th>Creación</th>
+                    <td><?=$video->created_at?></td>
+                </tr>
+                <tr>
+                    <th>Actualizado</th>
+                    <td><?=$video->updated_at?></td>
+                </tr>
+            </table>
+        </div>
     </div>
 <?= $this->endSection() ?>
